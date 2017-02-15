399b0e3bc2e87d55a457f03f81564087d7a20749
iocn-2-3-4 <iocn234@ukr.net>
2017-02-15 05:33:26 +0200

    Update testLinkedList.py

--- python_OOP/testLinkedList.py
+++ python_OOP/testLinkedList.py
@@ -1,5 +1,5 @@
 import unittest
-
+# import linkedList
 from linkedList import linkedList
 
 class TestLinkedList(unittest.TestCase):
@@ -11,54 +11,12 @@ class TestLinkedList(unittest.TestCase):
 
     def test_put(self):
         self.list.put("test1")
-        self.assertTrue(self.list.head.get_data() == "test1")
-        self.assertTrue(self.list.head.get_next() is None)
+        self.assertEqual(self.list.get(),"test1")
 
     def test_put_two(self):
         self.list.put("test1")
         self.list.put("test2")
-        self.assertTrue(self.list.head.get_data() == "test2")
-        head_next = self.list.head.get_next()
-        self.assertTrue(head_next.get_data() == "test1")
-
-    def test_nextNode(self):
-        self.list.put("test")
-        self.list.put("test2")
-        self.list.put("test3")
-
-        self.assertTrue(self.list.head.get_data() == "test3")
-
-        head_next = self.list.head.get_next()
-        self.assertTrue(head_next.get_data() == "test2")
-
-        last = head_next.get_next()
-        self.assertTrue(last.get_data() == "test")
-
-    def test_positive_get(self):
-        self.list.put("test")
-        self.list.put("test2")
-        self.list.put("test3")
-
-        found = self.list.get("test")
-        self.assertTrue(found.get_data() == "test")
-
-        found = self.list.get("test2")
-        self.assertTrue(found.get_data() == "test2")
-
-        found = self.list.get("test")
-        self.assertTrue(found.get_data() == "test")
-
-    def test_getNone(self):
-        self.list.put("test")
-        self.list.put("test2")
-
-        # make sure reg get works
-        found = self.list.get("test")
-
-        self.assertTrue(found.get_data() == "test")
-
-        with self.assertRaises(ValueError):
-            self.list.get("test")
+        self.assertTrue(self.list.get() == "test test2")
 
     def test_delete(self):
         self.list.put("test")
@@ -67,11 +25,11 @@ class TestLinkedList(unittest.TestCase):
 
         # Delete the list head
         self.list.delete("test3")
-        self.assertTrue(self.list.head.get_data() == "test2")
+        self.assertTrue(self.list.get == "test2")
 
         # Delete the list tail
         self.list.delete("test")
-        self.assertTrue(self.list.head.get_next() is None)
+        self.assertTrue(self.list.get is None)
 
     def test_delete_value_not_in_list(self):
         self.list.put("test")
@@ -85,11 +43,9 @@ class TestLinkedList(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.list.delete("WRONG")
 
-    def test_delete_next_reassignment(self):
-        self.list.put("test")
-        self.list.put("testing")
-        self.list.put("test2")
-        self.list.put("test3")
 
-        self.list.delete("test2")
-        self.list.delete("Cid")
+if __name__ == '__main__':
+    unittest.main()
+
+# self.list.put("test1")
+# self.list.get()
