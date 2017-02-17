1d03f08851f95b60eeb3f4b4a12c797424edcc89
iocn-2-3-4 <iocn234@ukr.net>
2017-02-17 06:44:59 +0200

    Update testLinkedList.py

--- python_OOP/testLinkedList.py
+++ python_OOP/testLinkedList.py
@@ -11,42 +11,51 @@ class TestLinkedList(unittest.TestCase):
 
     def test_put(self):
         self.list.put("test1")
-        self.assertEqual(self.list.get(),"test1")
+        self.assertEqual(self.list.get()," test1 ")
 
     def test_put_two(self):
         self.list.put("test1")
         self.list.put("test2")
-        self.assertTrue(self.list.get() == "test test2")
+        self.assertTrue(self.list.get() == " test1 test2 ")
 
     def test_delete(self):
-        self.list.put("test")
+        self.list.put("test1")
         self.list.put("test2")
         self.list.put("test3")
-
-        # Delete the list head
+        #delete tail
         self.list.delete("test3")
-        self.assertTrue(self.list.get == "test2")
+        self.assertTrue(self.list.get() == " test1 test2 ")
 
-        # Delete the list tail
-        self.list.delete("test")
-        self.assertTrue(self.list.get is None)
+        #delete  head
+        self.list.delete("test1")
+        self.assertTrue(self.list.get() ==  " test2 ")
 
     def test_delete_value_not_in_list(self):
         self.list.put("test")
         self.list.put("test2")
         self.list.put("test3")
-
-        with self.assertRaises(ValueError):
-            self.list.delete("WRONG")
+        try:
+            self.list.delete("test4")
+        except ValueError:
+            raise ValueError
+        with self.assertRaises(ValueError("Wrong element to delete")):
+            self.list.delete("test4")
 
     def test_delete_empty_list(self):
-        with self.assertRaises(ValueError):
-            self.list.delete("WRONG")
+        with self.assertRaises(ValueError('empty list')):
+            self.list.delete("test")
 
+    def test_size_of_list(self):
+        self.list.put("test")
+        self.list.put("test2")
+        self.list.put("test3")
+        self.assertTrue(self.list.size() == 3)
+
+    def test_index_of_item(self):
+        self.list.put("test")
+        self.list.put("test2")
+        self.list.put("test3")
+        self.assertTrue(self.list.indexOf("test2") == 2)
 
 if __name__ == '__main__':
     unittest.main()
-
-# self.list.put("test1")
-# self.list.get()
-
